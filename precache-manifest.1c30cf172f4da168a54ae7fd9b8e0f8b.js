e2d8d9d9790746640beb0825b3488ef101f44141
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2020-05-21 23:25:32 +0300

    Updates

---
+++ precache-manifest.1c30cf172f4da168a54ae7fd9b8e0f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04b23a1a0f945611c7f0440c4a034b9",
+    "url": "/Movie-Search-App/index.html"
+  },
+  {
+    "revision": "858ab3542469deeec33f",
+    "url": "/Movie-Search-App/static/css/main.6d5adeb5.chunk.css"
+  },
+  {
+    "revision": "9cb955b3089d5afd280e",
+    "url": "/Movie-Search-App/static/js/2.24080a62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Search-App/static/js/2.24080a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858ab3542469deeec33f",
+    "url": "/Movie-Search-App/static/js/main.c3ecfdb9.chunk.js"
+  },
+  {
+    "revision": "d027b3aacdb6c35c3036",
+    "url": "/Movie-Search-App/static/js/runtime-main.b8196268.js"
+  }
+]);
